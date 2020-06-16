9262a1e93b9ff354bb97884d7c74e682981d9168
ymk1t <yuma1102eff@gmail.com>
2020-06-17 01:03:53 +0900

    first commit

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        inputText.delegate = self
     }
 
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if let searchKey = textField.text {
+            print(searchKey)
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(searchKey,  completionHandler: {(placemarks, error) in
+            if let unwrapPlacemarks = placemarks {
+                if let firstPlacemark = unwrapPlacemarks.first {
+                    if let location = firstPlacemark.location {
+                        let targetCoodinate = location.coordinate
+                        print(targetCoodinate)
+                    }
+                }
+            }
+            })
+        }
+        return true
+    }
 
+    @IBAction func changeMapButton(_ sender: Any) {
+        if dispMap.mapType == .standard {
+            dispMap.mapType = .satellite
+        } else if dispMap.mapType == .satellite {
+            dispMap.mapType = .hybrid
+        } else if dispMap.mapType == .hybrid {
+            dispMap.mapType = .satelliteFlyover
+        } else if dispMap.mapType == .satelliteFlyover {
+            dispMap.mapType = .hybridFlyover
+        } else if dispMap.mapType == .hybridFlyover {
+            dispMap.mapType = .mutedStandard
+        } else {
+            dispMap.mapType = .standard
+        }
+    }
 }
 
